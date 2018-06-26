018a3471c35f8e621e526ab3b3bc080555873905
yearmfew <brlylmz23@gmail.com>
2018-06-26 12:41:01 +0300

    ürünler sayfası düzenleme
    ürünler sayfası ile durum kısmı düzenlendi. bir toggle yerleştirdik. ayrıca bir uyarı metni oluşturduk. bu işlemler sonunda artık veri tabanı işlemlerine geçeceğiz.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,10 +1,20 @@
 <div class="row">
 	<div class="col-md-12">
-		<h4 class="m-b-lg">Ürün Listesi</h4>
+		<h4 class="m-b-lg">
+			Ürün Listesi
+			<a href="#" class="btn btn-outline btn-primary btn-xs pull-right"> <i class="fa fa-plus"></i> Yeni Ekle</a>
+		</h4>
 	</div><!-- END column -->
 
 	<div class="col-md-12">
 		<div class="widget p-lg">
+
+			<div class="alert alert-info text-center">
+				
+				<p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="#" >tıklayınız</a></p>
+			</div>
+
+
 			<table class="table table-hover table-striped">
 				<thead>
 					<th>#id</th>
@@ -20,7 +30,9 @@
 						<td>monitor Askısı</td>
 						<td>menitor askısı</td>
 						<td>360 derece kullanılabilen monitor askısı</td>
-						<td>1/0</td>
+						<td>			
+							<input id="switch-2-2" type="checkbox" data-size="small"  data-switchery data-color="#10c469" checked />
+						</td>
 						<td>
 							<a href="#" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i>Sil</a>
 							<a href="#" class="btn btn-xs btn-info btn-outline"><i class="fa fa-pencil-square-o"></i>Düzenle</a>
