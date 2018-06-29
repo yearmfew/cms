c9797d7c351fec09efdb64a103f9019e1a0f74bb
yearmfew <brlylmz23@gmail.com>
2018-06-29 14:22:00 +0300

    hata mesajı düzenleme
    hata mesajı olduğu sayfada gösterilecek şekilde düzenlendi ve bir css dosyası oluşturuldu

--- panel/application/views/product_v/add/content.php
+++ panel/application/views/product_v/add/content.php
@@ -1,30 +1,28 @@
 <div class="row">
-	<div class="col-md-12">
-		<h4 class="m-b-lg">
-			Yeni Ürün Ekle
-		</h4>
-	</div><!-- END column -->
-
-	<div class="col-md-12">
-		<div class="widget">
-			
-					<div class="widget-body">
-						<form action = "<?php echo base_url("product/save"); ?>" method = "post" >
-							<div class="form-group">
-								<label for="exampleInputEmail1">Başlık</label>
-								<input name="title" class="form-control"  placeholder="Başlık" >
-							</div>
-							<div class="form-group">
-								<label>Açıklama</label>
-							<textarea name ="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
-				
-							</div>
-							
-							
-							<button type="submit" class="btn btn-primary btn-sm btn-outline">Kaydet</button>
-							<a href="<?php echo base_url("product"); ?> " class="btn btn-danger btn-sm  btn-outline">İptal Et</a>
-						</form>
-					</div><!-- .widget-body -->
-				</div>
-	</div><!-- END column -->
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+            Yeni Ürün Ekle
+        </h4>
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget">
+            <div class="widget-body">
+                <form action="<?php echo base_url("product/save"); ?>" method="post">
+                    <div class="form-group">
+                        <label>Başlık</label>
+                        <input class="form-control" placeholder="Başlık" name="title">
+                        <?php if(isset($form_error)){ ?> <!--  Eğer bir hata varsa mesaj yazdırıyoruz -->
+                            <small class="input-form-error"> <?php echo form_error("title"); ?></small>
+                        <?php } ?>
+                    </div>
+                    <div class="form-group">
+                        <label>Açıklama</label>
+                        <textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
+                    <a href="<?php echo base_url("product"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+                </form>
+            </div><!-- .widget-body -->
+        </div><!-- .widget -->
+    </div><!-- END column -->
 </div>
