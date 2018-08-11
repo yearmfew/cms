a9b910d7192c2ec9745d0d5fc88431d580a66989
yearmfew <brlylmz23@gmail.com>
2018-08-11 07:54:14 +0300

    brands

--- panel/application/views/brands_v/add/content.php
+++ panel/application/views/brands_v/add/content.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-12">
         <h4 class="m-b-lg">
-            Yeni Referans Ekle
+            Yeni Marka Ekle
         </h4>
     </div><!-- END column -->
     <div class="col-md-12">
@@ -15,18 +15,10 @@
                             <small class="pull-right input-form-error"> <?php echo form_error("title"); ?></small>
                         <?php } ?>
                     </div>
-                    <div class="form-group">
-                        <label>Açıklama</label>
-                        <textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
-                    <?php if(isset($form_error)){ ?>
-                            <small class="pull-right input-form-error"> <?php echo form_error("description"); ?></small>
-                        <?php } ?>
+                    <div class="form-group image_upload_container">
+                        <label>Logo Ekleyin</label>
+                        <input type="file" name="img_url" class="form-control">
                     </div>
-
-                        <div class="form-group image_upload_container">
-                            <label>Logo Ekleyin</label>
-                            <input type="file" name="img_url" class="form-control">
-                        </div>
                     <button type="submit" class="btn btn-primary btn-xs btn-outline">Kaydet</button>
                     <a href="<?php echo base_url("references"); ?>" class="btn btn-xs btn-danger btn-outline">İptal</a>
                 </form>
